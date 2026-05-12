ada36138e081571561512203ea3ce1aa2ab46f30
Harshith G R <harshithgrgowda555@gmail.com>
2026-05-12 21:42:18 +0530

    small fix: added domain name

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,6 +30,7 @@ _allowed_origins = [
     "http://localhost:5000",
     "http://0.0.0.0:5000",
     "http://127.0.0.1:5000",
+    "https://rx-detect.vercel.app",
 ]
 if _dev_domain:
     _allowed_origins += [
